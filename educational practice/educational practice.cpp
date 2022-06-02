ecc3399bdc140e57f364f3faa759d396ab2a8214
Boris_Bykov <borya.bikov@mail.ru>
2022-06-02 22:27:59 +0300

    v.2 Создание массива слов, удалеине лишнего

--- educational practice/educational practice.cpp
+++ educational practice/educational practice.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <fstream> // для работы с файлом
+#include <vector> // добавляет контейнер vector (динамический массив с индексами и своими функциями)
 #include <string> // упрощение работы со строками
 #include <cmath> // добавляет дополнительнные математические функции
 using namespace std;
@@ -29,6 +30,22 @@ int File_check(ifstream& file, ifstream& file_1, ofstream& file_analysis, ofstre
     return 0;
 }
 
+vector<string> Removal_of_excess(vector<string> words, string word, ifstream& file) // удаление знаков препинания и чисел, создание массива слов
+{
+    for (file >> word; !file.eof(); file >> word) { // создание массива слов
+        words.push_back(word);
+    }
+
+    for (int i = 0; i < words.size(); i++) { // удаление знаков препинания и чисел
+        for (int j = words[i].length(); j >= 0; j--) {
+            if (!(words[i][j] >= 'A' && words[i][j] <= 'Z' or words[i][j] >= 'a' && words[i][j] <= 'z' or words[i][j] == '\'')) {
+                words[i].erase(j, 1);
+            }
+        }
+    }
+    return words;
+}
+
 int main()
 {
     setlocale(LC_ALL, "rus");
@@ -39,6 +56,14 @@ int main()
     ofstream file_result("E:\\Учебная практика\\program\\result.txt");
 
     File_check(file, file_1, file_analysis, file_result);
+
+    vector<string> words;
+    string word;
+    words = Removal_of_excess(words, word, file);
+
+    for (int i = 0; i < words.size(); i++) {
+        cout << words[i] << endl;
+    }
 
     file.close();
     file_1.close();
